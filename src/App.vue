6f6d9095b9b658d5e0bde07ed6f69c7272497a45
VranceanuM <markdevstudio@gmail.com>
2017-12-29 17:20:26 +0300

    navbar

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+<v-app>
+  <v-toolbar dark class="light-blue lighten-2">
+    <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+     <v-toolbar-title>Kiev Meetup</v-toolbar-title>
+     <v-spacer></v-spacer>
+     <v-toolbar-items class="hidden-xs-only">
+       <v-btn flat>
+         <v-icon>supervisor_account</v-icon>
+         View Meetups</v-btn>
+     </v-toolbar-items>
+  </v-toolbar>
+    <v-navigation-drawer
+    v-model="drawer"
+    hide-overlay:true
+    height:100
+    light
+    temporary
+    absolute
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+   </v-navigation-drawer>
+    <main>
+ 
+    </main>
+</v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+       drawer:false
+      }
+    }
+  }
+</script>
+<style scoped>
+
+h1{
+  color:accent;
+}
+</style>
